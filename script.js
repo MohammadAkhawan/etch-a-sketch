c7f98e59e5b7d5d138baaa1641bf87d6e0f99d69
mammad <113013285+MohammadAkhawan@users.noreply.github.com>
2024-02-28 13:58:42 -0500

    Update the DOM to generate table based on requested number

--- script.js
+++ script.js
@@ -3,33 +3,41 @@ const generateBtnElement = document.querySelector("#generate");
 
 const bigContainerElement = document.querySelector(".container");
 
+const emptyDiv = bigContainerElement.innerHTML;
+
 const checkNumberIsValid = (input) => {
     if (userInputElement.value === "") {
         alert("You Should Enter a Number!");
         return false;
-    } else if (input > 100 || input < 1) {
-        alert("You Should Enter a Number Between 1 and 100");
+    } else if (input > 100 || input < 2) {
+        alert("You Should Enter a Number Between 2 and 100");
         return false;
     }
     return true;
 };
 
-const emptyDiv = bigContainerElement.innerHTML;
+const setElementsOfTable = (size) => {
+    let tableSize = size ** 2;
+    while (tableSize > 0) {
+        const divElement = document.createElement("div");
+        divElement.classList.add("the-divs");
+        let divWidth = 960 / size;
+        divElement.style.width = `${divWidth}px`;
+        bigContainerElement.appendChild(divElement);
+        tableSize--;
+    }
+};
 
 const generateTable = () => {
     let userInput = Number(userInputElement.value);
 
     if (checkNumberIsValid(userInput)) {
         bigContainerElement.innerHTML = emptyDiv;
-        let userInputNumber = userInput ** 2;
-        while (userInputNumber > 0) {
-            const divElement = document.createElement("div");
-            divElement.classList.add("the-divs");
-            bigContainerElement.appendChild(divElement);
-            userInputNumber--;
-        }
-        userInputElement.value = "";
+
+        setElementsOfTable(userInput);
     }
+    userInputElement.value = "";
+    userInputElement.focus();
 };
 
 generateBtnElement.addEventListener("click", generateTable);

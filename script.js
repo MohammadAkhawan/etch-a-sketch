17a224fea4d68574f004117ccbb17ce867c8083c
mammad <113013285+MohammadAkhawan@users.noreply.github.com>
2024-02-28 12:59:43 -0500

    Setup the DOM to generate the requested number of divs.

--- script.js
+++ script.js
@@ -3,4 +3,33 @@ const generateBtnElement = document.querySelector("#generate");
 
 const bigContainerElement = document.querySelector(".container");
 
+const checkNumberIsValid = (input) => {
+    if (userInputElement.value === "") {
+        alert("You Should Enter a Number!");
+        return false;
+    } else if (input > 100 || input < 1) {
+        alert("You Should Enter a Number Between 1 and 100");
+        return false;
+    }
+    return true;
+};
 
+const emptyDiv = bigContainerElement.innerHTML;
+
+const generateTable = () => {
+    let userInput = Number(userInputElement.value);
+
+    if (checkNumberIsValid(userInput)) {
+        bigContainerElement.innerHTML = emptyDiv;
+        let userInputNumber = userInput ** 2;
+        while (userInputNumber > 0) {
+            const divElement = document.createElement("div");
+            divElement.classList.add("the-divs");
+            bigContainerElement.appendChild(divElement);
+            userInputNumber--;
+        }
+        userInputElement.value = "";
+    }
+};
+
+generateBtnElement.addEventListener("click", generateTable);

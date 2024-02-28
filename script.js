d42111f3f180ce3c91336572003c760c02fc7077
mammad <113013285+MohammadAkhawan@users.noreply.github.com>
2024-02-28 12:57:57 -0500

    Setup the header, input and generate button

---
+++ script.js
@@ -0,0 +1,6 @@
+const userInputElement = document.querySelector("#user-input");
+const generateBtnElement = document.querySelector("#generate");
+
+const bigContainerElement = document.querySelector(".container");
+
+
